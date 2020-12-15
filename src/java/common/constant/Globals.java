3335ae1f3d5a8c12f8e3f4c900bebeadbee398a0
ebubekir <ebubekir@172.30.16.99>
2020-12-16 00:32:25 +0100

    completed login page

--- src/java/common/constant/Globals.java
+++ src/java/common/constant/Globals.java
@@ -3,9 +3,9 @@ package common.constant;
 import java.util.Locale;
 
 public class Globals {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
     public static final String VERSION = "1.001";
-    public static final String APP_NAME = "Kurumsal";
+    public static final String APP_NAME = "Recruitment";
     public static final Locale LOCALE = new Locale("tr", "TR");
     public static final String DATE_FORMAT_DB = "yyyyMMdd";
     public static final String DATE_TIME_FORMAT_SCREEN = "dd/MM/yyyy HH:mm";
